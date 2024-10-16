7f74f07f48b9f9366dcf04acdadbfb1d2a8b781a
Priscylla <Priscylla.Delta@gmail.com>
2024-10-15 19:50:54 -0500

    Add files via upload

--- Change-making.py
+++ Change-making.py
@@ -1,25 +1,28 @@
+from decimal import Decimal
 
 
+money = 100.58
 
-money = 10
+coins = [100, 50, 20, 10, 5, 1, .25, .10, .05, .01]
 
-coins = [1,2,3,4,5]
-coins.reverse()
 print(coins)
 
 
 
-
+used_Coins = []
 i = 0
+print(coins[i] <= money)
 while money > 0:
     if coins[i] <= money:
-        money = money - coins[i]
-        print(coins[i])
+        money = round(money - (coins[i]), ndigits=2)
+        used_Coins.append(coins[i])
+        ## print(money)
+        ## print(coins[i])
     else:
         i += 1
 
 
-
+print(used_Coins)
 print("Leftover money =" , money)
 
 
