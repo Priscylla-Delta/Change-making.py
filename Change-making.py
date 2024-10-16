cc989fd32a054d1f9921f97dfc6a34d1c9c3f168
Priscylla <Priscylla.Delta@gmail.com>
2024-10-15 21:16:05 -0500

    Add files via upload

--- Change-making.py
+++ Change-making.py
@@ -1,10 +1,11 @@
 import sys
+import math
 
 if len(sys.argv) == 2:
     money = sys.argv[1]
     try:
-        value = float(money)
-        if value > 0:
+        money = round(float(money),ndigits=2) #cuts the input to 2 sd
+        if money > 0:
             pass
         else:
             print("Posative numbers only")
@@ -17,7 +18,7 @@ if len(sys.argv) == 2:
 
     has_chosen = False
     while has_chosen == False:
-        print("1) Standard USD? \n2) Prime Numbers up to the input? \n3) Manually input a list of coin values?")
+        print("1) Standard USD? \n2) Prime Numbers up to the input? (This selection will use Standard USD for any decimal values)\n3) Manually input a list of coin values?")
         coin_selection = input()
         
         if coin_selection in ["1","2","3"]:
@@ -29,8 +30,27 @@ if len(sys.argv) == 2:
             coins = [100, 50, 20, 10, 5, 1, .25, .10, .05, .01]
         elif coin_selection == "2":
             coins = []
-            print("primes still needs to be setup")
-            sys.exit()
+            #print("primes still needs to be setup")
+
+
+            #chat, I have no idea how this works, but it does, so thats neat
+                # Method 5 : https://stackoverflow.com/questions/11619942/print-series-of-prime-numbers-in-python
+            coins = list()
+            limit = math.floor(money)
+            sieve = [True] * (limit + 1)
+            for p in range(2, limit + 1):
+                if (sieve[p]):
+                    coins.append(p)
+                    for i in range(p, limit + 1, p):
+                        sieve[i] = False
+
+            decimal_coins = [.25, .1, .05, .01]
+            for extra in decimal_coins:
+                coins.append(extra)
+
+            coins.sort(reverse=True)
+            # print(coins)
+    
         else: 
             coins = []
             print("Manual still needs to be setup")
@@ -50,7 +70,7 @@ while money > 0:
     if coins[i] <= money:
         money = round(money - (coins[i]), ndigits=2)
         used_Coins.append(coins[i])
-        ## print(money)
+        # print(money)
         ## print(coins[i])
     else:
         i += 1
