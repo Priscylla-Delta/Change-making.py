c9b580e1c3a5fb4dcf4b8ce6ffe74e8f5f6a813c
Priscylla <Priscylla.Delta@gmail.com>
2024-10-15 20:06:06 -0500

    Add files via upload

--- Change-making.py
+++ Change-making.py
@@ -5,13 +5,12 @@ money = 100.58
 
 coins = [100, 50, 20, 10, 5, 1, .25, .10, .05, .01]
 
-print(coins)
+#print(coins)
 
 
 
 used_Coins = []
 i = 0
-print(coins[i] <= money)
 while money > 0:
     if coins[i] <= money:
         money = round(money - (coins[i]), ndigits=2)
@@ -22,7 +21,17 @@ while money > 0:
         i += 1
 
 
-print(used_Coins)
+counts = {}
+for used_Coin in used_Coins:
+    count = used_Coins.count(used_Coin)
+    counts[used_Coin] = count
+    #print(used_Coin, "was used", count, "times")
+
+for coin in counts:
+    print(coin, "was used", counts[coin], "times")
+
+
+#print(counts)
 print("Leftover money =" , money)
 
 
